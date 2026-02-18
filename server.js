7f6fb801474efd25f3deca047f4b33f338471bd2
jfdelgado82 <128656711+jfdelgado82@users.noreply.github.com>
2026-02-18 08:32:40 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const repo = 'UMARMultilingua';
 const path = 'chatino.json';
 const token = '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'; // Mantener seguro en backend
 const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+//Git Kraken token: eJwtzLFuwjAQgOF3udlD08bBeOtQMQEdqFR1sc72OVgxsXVOoIB4dySU9Ze+/w7oHNV6yAONoKGV0qLEVkmnnH+z0qrmA73vWre2dhVCoxrlVx0IKJzP0RMv9L6czLSs+mM275Pfh8M2xU33hfT3fe6OfZWfu59b/1v3rbrFIEHAi5jpWgg0WEImBgHV5VdAf4qjLrNN0ZmBroIJvc7cC6aSxVyJNZ0wJnHJPISULyCA/ktkqgYn0OOc0uPxBNp7UOQ=
+//eJwtzLFuwjAQgOF3udlD08bBeOtQMQEdqFR1sc72OVgxsXVOoIB4dySU9Ze+/w7oHNV6yAONoKGV0qLEVkmnnH+z0qrmA73vWre2dhVCoxrlVx0IKJzP0RMv9L6czLSs+mM275Pfh8M2xU33hfT3fe6OfZWfu59b/1v3rbrFIEHAi5jpWgg0WEImBgHV5VdAf4qjLrNN0ZmBroIJvc7cC6aSxVyJNZ0wJnHJPISULyCA/ktkqgYn0OOc0uPxBNp7UOQ=
+
 
 // Obtener SHA y contenido del archivo
 async function obtenerArchivo() {
@@ -27,6 +30,10 @@ async function obtenerArchivo() {
     const data = JSON.parse(content);
     return { data, sha };
 }
+
+app.get('/', (req, res) => {
+    res.send('Backend funcionando correctamente 🚀');
+  });
 
 // Endpoint GET: Leer diccionario
 app.get('/diccionario', async (req, res) => {
